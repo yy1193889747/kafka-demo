648168262ec4972c6bf66ed57f992965c950e565
congyang guo <congyangguo@congyangdeiMac.local>
2018-08-30 17:31:40 +0800

    producer

--- src/main/java/com/cy/kafkademo/product/Producer.java
+++ src/main/java/com/cy/kafkademo/product/Producer.java
@@ -1,4 +1,39 @@
 package com.cy.kafkademo.product;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.util.Properties;
+
+/**
+ * @author congyang.guo
+ **/
 public class Producer {
+
+    public static void main(String[] args) {
+
+        Properties properties = new Properties();
+        properties.put("bootstrap.servers", "bill:9092,bill:9093,bill:9094");
+        properties.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        properties.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        ProducerRecord<String, String> record = new ProducerRecord<>("test", "hello", "world");
+        try {
+            producer.send(record).get();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+
+
 }
+
+
+
+
+
